391c28c8cfd9e63a818ab16f6604cfcefb98e6bb
Prasad's   Professional <PGProfessional@Prasads-MacBook-Pro.local>
2026-04-12 18:27:41 +0530

    login script without framework structure

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,19 +1,29 @@
 describe('View IQ platform Test Suite', () => {
-  it('Connecting to the Mailosaur and getting the login code', () => {
-    cy.env(['MAILOSAUR_API_KEY', 'MAILOSAUR_SERVER_ID']).then((env) => {
-      getLoginCodeFromMailosaur(env.MAILOSAUR_SERVER_ID).then((message) => {
-        return message.html.codes[0].value
-      }).as('login_code')
-    })
-    cy.get('@login_code').then((code) => {
-      cy.log(`OTP: ${code}`)
+  it('Get and print login code', () => {
+    const applicationURL = Cypress.expose('BASE_URL')
+    cy.env(['MAILOSAUR_API_KEY', 'MAILOSAUR_SERVER_ID', 'PASSWORD']).then((env) => {
+      const email = `challenge@${env.MAILOSAUR_SERVER_ID}.mailosaur.net`
+      cy.mailosaurDeleteAllMessages(env.MAILOSAUR_SERVER_ID);
+      cy.visit(applicationURL)
+      cy.get('#userName').type(email)
+      cy.get('#password').type(env.PASSWORD)
+      cy.get('#login-btn').click()
+      cy.wait(2000)
+      cy.contains('label', 'Email')
+        .prev('input[type="radio"]')
+        .should('be.checked');
+      cy.get('[data-testid="next-button-main"]').click()
+      cy.wait(2000)
+      cy.getLoginCode(env.MAILOSAUR_SERVER_ID, email).then((otp) => {
+        cy.log(`OTP: ${otp}`)
+        otp = otp.replaceAll("-", "");
+        otp.split('').forEach((digit, index) => {
+          // Replace '.digit-input' with your actual CSS selector
+          cy.get('.code-input')
+            .eq(index)
+            .type(digit);
+        })
+      })
     })
   })
-
-  //function
-  const getLoginCodeFromMailosaur = (serverID) => {
-    return cy.mailosaurGetMessage(serverID, {
-      sentTo: `challenge@${serverID}.mailosaur.net`,
-    })
-  }
-})
+}) 
